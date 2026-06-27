c97c1345fec12cba02883513f62f7d8dde06cb4f
Hitesh Kumar <hiteshkumar20132@gmail.com>
2026-06-27 09:57:26 +0530

    Remove duplicate folders from zip extraction; ignore future zip drops

--- connectsphere-bookmarks/connectsphere-backend/src/routes/bookmark.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { protect } = require('../middleware/auth');
-const c = require('../controllers/bookmark.controller');
-
-router.get('/', protect, c.getSavedPosts);
-router.post('/:postId', protect, c.savePost);
-router.delete('/:postId', protect, c.unsavePost);
-
-module.exports = router;
